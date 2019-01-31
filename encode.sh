ed05e8598bfc261ec8e83c0cded06bd1c6240082
Acehcyber123 <45812650+Acehcyber123@users.noreply.github.com>
2019-01-31 20:38:57 +0700

    Hack

---
+++ encode.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#rd3
+#slep
+echo -n "siapa nama kamu/your name ? :";
+read "nama";
+
+slep 3,2
+
+   echo "hi $nama, tunggu hingga penginstalan selesai ";
+ 
+     
+toilet -f mono12 -F gay package Termux
+               echo "install"
+              pkg install python
+            clear
+         pkg install php
+       clear
+#install pip
+     pip2 install mechanize
+            clear
+#install git -get -install
+           apt install git
+          clear
+          apt install python2 
+        clear
+       pkg install toilet 
+      clear 
+     pkg install nano
+    clear
+   pkg install proot
+  clear 
+ pkg install git
+               clear
+               pkg install nano 
+                              clear 
+            pip 2 install requests
+          clear 
+         apt update && apt upgrade 
+       clear
+        exit
+
+          Y
+
+done!!!!
